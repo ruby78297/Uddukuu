344411d8a137ada0e59b347221c24f5a80b30cc7
ruby78297 <rrubyrana8@gmail.com>
2021-08-13 16:52:15 +0530

    update

--- src/Components/SectionBreaker/SectionBreakjer2.js
+++ src/Components/SectionBreaker/SectionBreakjer2.js
@@ -4,7 +4,29 @@ import "./SectionBreaker2.css";
 const ServiceBreaker2 = () => {
   return (
     <div className="row section-Wraper">
-      <div className=" section-Wraper-1"></div>
+      <div className=" section-Wraper-1">
+        <div>
+          <h1 className="section-Wraper-0-content">How Udukku works</h1>
+        </div>
+        <div>
+          <h3 className="section-Wraper-1-content Wraper-1-content ">
+            {" "}
+            Trust Udukku, We will give a shape to your telents{" "}
+          </h3>
+        </div>
+        <div>
+          <h3 className="section-Wraper-1-content  Wraper-2-content">
+            {" "}
+            trust Udukku, We will give a shape to your telents{" "}
+          </h3>
+        </div>
+        <div>
+          <h3 className="section-Wraper-1-content   Wraper-3-content ">
+            {" "}
+            trust Udukku, We will give a shape to your telents{" "}
+          </h3>
+        </div>
+      </div>
 
       <div className="col-sm section-breaker-image"></div>
     </div>
