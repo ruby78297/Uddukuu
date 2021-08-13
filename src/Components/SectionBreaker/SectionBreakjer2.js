e374fe2134c0d197c99adc4ef2266f365f170d87
ruby78297 <rrubyrana8@gmail.com>
2021-08-13 13:18:54 +0530

    updated

--- src/Components/SectionBreaker/SectionBreakjer2.js
+++ src/Components/SectionBreaker/SectionBreakjer2.js
@@ -4,14 +4,9 @@ import "./SectionBreaker2.css";
 const ServiceBreaker2 = () => {
   return (
     <div className="row section-Wraper">
-      <div className=" section-Wraper-1">
-        <div className=" section-Wraper-2">
-          <div className="col-sm breaker-image"></div>
-          <div className="breaker">
-            <h3>lorem loremloremloremlorem</h3>
-          </div>
-        </div>
-      </div>
+      <div className=" section-Wraper-1"></div>
+
+      <div className="col-sm section-breaker-image"></div>
     </div>
   );
 };
